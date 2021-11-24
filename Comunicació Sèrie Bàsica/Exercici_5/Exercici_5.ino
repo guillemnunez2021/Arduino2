e80f1c90f59c1116fa8d938bb34fac213f2b1242
guillemnunez2021 <guillem.nunez.2130@lacetania.cat>
2021-11-24 19:20:50 +0100

    Exercicis

---
+++ Comunicació Sèrie Bàsica/Exercici_5/Exercici_5.ino
@@ -0,0 +1,14 @@
+void setup() {
+  // put your setup code here, to run once:
+Serial.begin(9600);
+Serial.println("Escull el numero de l'operació que vols realitzar?");
+Serial.println("1.Comprovar tarjeta de credit");
+Serial.println("2.Comprova el numero del compte bancari");
+Serial.println("3.Busca el digit perdut de la tarjeta de credit");
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
