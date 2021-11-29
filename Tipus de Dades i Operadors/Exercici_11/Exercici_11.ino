5dd415606a47bde54c964751ed8d83b0bed3ee3b
guillemnunez2021 <guillem.nunez.2130@lacetania.cat>
2021-11-29 16:34:26 +0100

    11

--- Tipus de Dades i Operadors/Exercici_11/Exercici_11.ino
+++ Tipus de Dades i Operadors/Exercici_11/Exercici_11.ino
@@ -1,9 +1,39 @@
-void setup() {
-  // put your setup code here, to run once:
+/**********************************************************************************
+**                                                                               **
+**                               Math Is Fun                                     **
+**                                                                               **
+**                                                                               **
+**********************************************************************************/
 
+//********** Includes *************************************************************
+
+//********** Variables ************************************************************
+int drive_gb = 100;
+long drive_mb, drive_kb, real_drive_mb, real_drive_kb, missing_drive_kb;
+
+//********** Setup ****************************************************************
+void setup()
+{
+  Serial.begin(9600);
+  
+  Serial.print("Your HD is");
+  Serial.print(drive_gb);
+  Serial.println("GB large");
+  drive_mb = drive_gb;
+  drive_mb = 1024 * drive_mb;
+  Serial.print("In theory,");
+ Serial.print("it can store");
+ Serial.print(drive_mb);
+ Serial.print("Megabytes,");
+ drive_kb = drive_mb* pow(1024,2);
+ Serial.println("Kilobytes.")
+ 
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-
+//********** Loop *****************************************************************
+void loop()
+{
 }
+
+//********** Funcions *************************************************************
+ 
