3b873be7d7968a1fa84436696adbdae203dd2516
guillemnunez2021 <guillem.nunez.2130@lacetania.cat>
2021-12-03 15:36:07 +0100

    exercici 5

---
+++ Estructures de Control de Selecció if & switch/Exercici_5/Exercici_5.ino
@@ -0,0 +1,51 @@
+/**********************************************************************************
+**                                                                               **
+**                          Control Structures                                   **
+**                                                                               **
+**                                                                               **
+**********************************************************************************/
+
+//********** Includes *************************************************************
+
+//********** Variables ************************************************************
+float Indicador = 3.5;
+
+//********** Setup ****************************************************************
+void setup()              // run once, when the sketch starts
+{
+Serial.begin(9600);     // set up Serial library at 9600 bps
+if (Indicador < 3.5)
+{
+Serial.println("Rang = A");
+}
+else if (Indicador < 6.5 && Indicador >= 3.5)  
+{
+Serial.println("Rang = B ");
+}
+else if (Indicador < 11.1 && Indicador >= 6.5)
+{
+Serial.println("Rang = C"); 
+}
+else if (Indicador < 17.7 && Indicador >= 11.1) 
+{
+Serial.println("Rang = D");
+}
+else if (Indicador < 38.2 && Indicador >= 17.7)
+{
+  Serial.println("Rang = E");
+}
+else if (Indicador < 43.2 && Indicador >= 38.2)
+{
+  Serial.println("Rang = F");
+}
+else;
+Serial.println("Rang = G");
+}
+//********** Loop *****************************************************************
+void loop()   // we need this to be here even though its empty
+{
+  
+
+}
+
+//********** Funcions *************************************************************
