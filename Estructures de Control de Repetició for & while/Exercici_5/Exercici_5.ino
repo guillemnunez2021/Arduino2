ee190e72c51a87a07d12a1aa2aa624a62045785b
guillemnunez2021 <guillem.nunez.2130@lacetania.cat>
2021-12-10 15:41:09 +0100

    exercici 4 i 5

--- Estructures de Control de Repetició for & while/Exercici_5/Exercici_5.ino
+++ Estructures de Control de Repetició for & while/Exercici_5/Exercici_5.ino
@@ -8,13 +8,20 @@
 //********** Includes *************************************************************
 
 //********** Variables ************************************************************
-int taula = 3;
+int taula = 2;
 
 //********** Setup ****************************************************************
 void setup()      // run once, when the sketch starts
 {
   Serial.begin(9600);     // set up Serial library at 9600 bps
-  Serial.print("Taula de multiplicar del ");
+ 
+  }
+
+//********** Loop *****************************************************************
+void loop()   // we need this to be here even though its empty
+{
+   Serial.print("Taula de multiplicar del ");
+   taula = taula + 1;
   Serial.println(taula);
   for (int i=0; i <= 10; i++)
   {
@@ -23,13 +30,8 @@ void setup()      // run once, when the sketch starts
     Serial.print(" x ");
     Serial.print(i);
     Serial.print( " = ");
-    Serial.println(taula*i);    
+    Serial.println(taula*i);  
   }
+  
 }
-
-//********** Loop *****************************************************************
-void loop()   // we need this to be here even though its empty
-{
-}
-
 //********** Funcions *************************************************************
