7de51e50a53c4f69af2746ce36f88ec017e2e39e
guillemnunez2021 <guillem.nunez.2130@lacetania.cat>
2021-12-20 16:09:26 +0100

    EXERCICI1

---
+++ Display de 7 segments/Exercici_1/Exercici_1.ino
@@ -0,0 +1,95 @@
+        
+const byte segA = 7;     
+const byte segB = 6; 
+const byte segC = 4; 
+const byte segD = 2; 
+const byte segE = 3; 
+const byte segF = 9; 
+const byte segG = 10; 
+const byte segDP = 5; 
+void setup() {
+  // put your setup code here, to run once:
+Serial.begin(9600);
+pinMode(segA, OUTPUT);
+pinMode(segB, OUTPUT);
+pinMode(segC, OUTPUT);
+pinMode(segD, OUTPUT);
+pinMode(segE, OUTPUT);
+pinMode(segF, OUTPUT);
+pinMode(segG, OUTPUT);
+pinMode(segDP, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+//lletra g
+digitalWrite(segA, HIGH);
+digitalWrite(segB, HIGH);
+digitalWrite(segC, HIGH);
+digitalWrite(segD, HIGH);
+digitalWrite(segE, LOW);
+digitalWrite(segF, HIGH);
+digitalWrite(segG, HIGH);
+digitalWrite(segDP, LOW);
+delay(1000); 
+//lletra u
+digitalWrite(segA, LOW);
+digitalWrite(segB, HIGH);
+digitalWrite(segC, HIGH);
+digitalWrite(segD, HIGH);
+digitalWrite(segE, HIGH);
+digitalWrite(segF, HIGH);
+digitalWrite(segG, LOW);
+digitalWrite(segDP, LOW);
+delay(1000);
+//lletra i
+digitalWrite(segA, LOW);
+digitalWrite(segB, LOW);
+digitalWrite(segC, LOW);
+digitalWrite(segD, LOW);
+digitalWrite(segE, HIGH);
+digitalWrite(segF, HIGH);
+digitalWrite(segG, LOW);
+digitalWrite(segDP, LOW);
+delay(1000);
+//lletra l
+digitalWrite(segA, LOW);
+digitalWrite(segB, LOW);
+digitalWrite(segC, LOW);
+digitalWrite(segD, HIGH);
+digitalWrite(segE, HIGH);
+digitalWrite(segF, HIGH);
+digitalWrite(segG, LOW);
+digitalWrite(segDP, LOW);
+delay(1000);
+//lletra l
+digitalWrite(segA, LOW);
+digitalWrite(segB, LOW);
+digitalWrite(segC, LOW);
+digitalWrite(segD, HIGH);
+digitalWrite(segE, HIGH);
+digitalWrite(segF, HIGH);
+digitalWrite(segG, LOW);
+digitalWrite(segDP, LOW);
+delay(1000);
+//lletra e
+digitalWrite(segA, HIGH);
+digitalWrite(segB, LOW);
+digitalWrite(segC, LOW);
+digitalWrite(segD, HIGH);
+digitalWrite(segE, HIGH);
+digitalWrite(segF, HIGH);
+digitalWrite(segG, HIGH);
+digitalWrite(segDP, LOW);
+delay(1000);
+//lletra M
+digitalWrite(segA, HIGH);
+digitalWrite(segB, LOW);
+digitalWrite(segC, HIGH);
+digitalWrite(segD, LOW);
+digitalWrite(segE, HIGH);
+digitalWrite(segF, LOW);
+digitalWrite(segG, LOW);
+digitalWrite(segDP, LOW);
+delay(1000);
+}
--- Estructures de Control de Repetició for & while/Exercici_1/Exercici_1.ino
+++ Estructures de Control de Repetició for & while/Exercici_1/Exercici_1.ino
@@ -1,14 +1,17 @@
-int comptar = 11;
+int comptar = 5;
 void setup()                // run once, when the sketch starts
 {
   Serial.begin(9600);       // set up Serial library at 9600 bps
-  Serial.print("Ara comptare de 0 a ");
-  Serial.println(comptar);
+  
+ 
   for (int i=0; i <= comptar; i++)
   {
     Serial.print(i);
+   if ( i < (comptar-1))
+   {
     Serial.print("-");
   } 
+}
 }
 void loop()     // we need this to be here even though its empty
 {
