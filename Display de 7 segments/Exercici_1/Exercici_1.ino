472061c01818397951270b29da779919a377b765
guillemnunez2021 <guillem.nunez.2130@lacetania.cat>
2022-01-09 19:05:51 +0100

    DEFINICIONS EXERCICI 1

--- Display de 7 segments/Exercici_1/Exercici_1.ino
+++ Display de 7 segments/Exercici_1/Exercici_1.ino
@@ -1,95 +1,103 @@
-        
-const byte segA = 7;     
-const byte segB = 6; 
-const byte segC = 2; 
-const byte segD = 3; 
-const byte segE = 4; 
-const byte segF = 9; 
-const byte segG = 10; 
-const byte segDP = 5; 
+/**********************************************************************************
+**                                                                               **
+**                           Display 7 segments                                  **
+**                                                                               **
+**********************************************************************************/
+//********** Includes *************************************************************
+//********** Variables ************************************************************          
+const byte segA = 7; // donar nom al pin 7 de l’Arduino     
+const byte segB = 6;  // donar nom al pin 6 de l’Arduino
+const byte segC = 2;  // donar nom al pin 2 de l’Arduino
+const byte segD = 3;  // donar nom al pin 3 de l’Arduino
+const byte segE = 4;  // donar nom al pin 4 de l’Arduino
+const byte segF = 9;  // donar nom al pin 9 de l’Arduino
+const byte segG = 10;  // donar nom al pin 10 de l’Arduino
+const byte segDP = 5; // donar nom al pin 5 de l’Arduino
+//********** Setup ****************************************************************
 void setup() {
   // put your setup code here, to run once:
 Serial.begin(9600);
-pinMode(segA, OUTPUT);
-pinMode(segB, OUTPUT);
-pinMode(segC, OUTPUT);
-pinMode(segD, OUTPUT);
-pinMode(segE, OUTPUT);
-pinMode(segF, OUTPUT);
-pinMode(segG, OUTPUT);
-pinMode(segDP, OUTPUT);
+pinMode(segA, OUTPUT);// definir el pin 7 com una sortida
+pinMode(segB, OUTPUT);// definir el pin 6 com una sortida
+pinMode(segC, OUTPUT);// definir el pin 2 com una sortida
+pinMode(segD, OUTPUT);// definir el pin 3 com una sortida
+pinMode(segE, OUTPUT);// definir el pin 4 com una sortida
+pinMode(segF, OUTPUT);// definir el pin 9 com una sortida
+pinMode(segG, OUTPUT);// definir el pin 10 com una sortida
+pinMode(segDP, OUTPUT);// definir el pin 5 com una sortida
 }
+//********** Loop *****************************************************************
 
 void loop() {
   // put your main code here, to run repeatedly:
 //lletra g
-digitalWrite(segA, HIGH);
-digitalWrite(segB, HIGH);
-digitalWrite(segC, HIGH);
-digitalWrite(segD, HIGH);
-digitalWrite(segE, LOW);
-digitalWrite(segF, HIGH);
-digitalWrite(segG, HIGH);
-digitalWrite(segDP, LOW);
-delay(1000); 
+digitalWrite(segA, HIGH);//segment A high
+digitalWrite(segB, HIGH);//segment B high
+digitalWrite(segC, HIGH);//segment E high
+digitalWrite(segD, HIGH);//segment D high
+digitalWrite(segE, LOW);//segment E high
+digitalWrite(segF, HIGH);//segment F high
+digitalWrite(segG, HIGH);//segment G high
+digitalWrite(segDP, LOW);//segment DP high
+delay(1000); //segment A high
 //lletra u
-digitalWrite(segA, LOW);
-digitalWrite(segB, HIGH);
-digitalWrite(segC, HIGH);
-digitalWrite(segD, HIGH);
-digitalWrite(segE, HIGH);
-digitalWrite(segF, HIGH);
-digitalWrite(segG, LOW);
-digitalWrite(segDP, LOW);
+digitalWrite(segA, LOW);//segment A high
+digitalWrite(segB, HIGH);//segment B high
+digitalWrite(segC, HIGH);//segment C high
+digitalWrite(segD, HIGH);//segment D high
+digitalWrite(segE, HIGH);//segment E high
+digitalWrite(segF, HIGH);//segment F high
+digitalWrite(segG, LOW);//segment G high
+digitalWrite(segDP, LOW);//segment DP high
 delay(1000);
 //lletra i
-digitalWrite(segA, LOW);
-digitalWrite(segB, LOW);
-digitalWrite(segC, LOW);
-digitalWrite(segD, LOW);
-digitalWrite(segE, HIGH);
-digitalWrite(segF, HIGH);
-digitalWrite(segG, LOW);
-digitalWrite(segDP, LOW);
-delay(1000);
+digitalWrite(segA, LOW);//segment A high
+digitalWrite(segB, LOW);//segment B high
+digitalWrite(segC, LOW);//segment C high
+digitalWrite(segD, LOW);//segment D high
+digitalWrite(segE, HIGH);//segment E high
+digitalWrite(segF, HIGH);//segment F high
+digitalWrite(segG, LOW);//segment G high
+digitalWrite(segDP, LOW);//segment DP high
+delay(1000);//segment A high
 //lletra l
-digitalWrite(segA, LOW);
-digitalWrite(segB, LOW);
-digitalWrite(segC, LOW);
-digitalWrite(segD, HIGH);
-digitalWrite(segE, HIGH);
-digitalWrite(segF, HIGH);
-digitalWrite(segG, LOW);
-digitalWrite(segDP, LOW);
-delay(1000);
+digitalWrite(segA, LOW);//segment A high
+digitalWrite(segB, LOW);//segment B high
+digitalWrite(segC, LOW);//segment C high
+digitalWrite(segD, HIGH);//segment D high
+digitalWrite(segE, HIGH);//segment E high
+digitalWrite(segF, HIGH);//segment F high
+digitalWrite(segG, LOW);//segment G high
+digitalWrite(segDP, LOW);//segment DP high
+delay(1000);//segment A high
 //lletra l
-digitalWrite(segA, LOW);
-digitalWrite(segB, LOW);
-digitalWrite(segC, LOW);
-digitalWrite(segD, HIGH);
-digitalWrite(segE, HIGH);
-digitalWrite(segF, HIGH);
-digitalWrite(segG, LOW);
-digitalWrite(segDP, LOW);
-delay(1000);
+digitalWrite(segA, LOW);//segment A high
+digitalWrite(segB, LOW);//segment B high
+digitalWrite(segC, LOW);//segment C high
+digitalWrite(segD, HIGH);//segment D high
+digitalWrite(segE, HIGH);//segment E high
+digitalWrite(segF, HIGH);//segment F high
+digitalWrite(segG, LOW);//segment G high
+digitalWrite(segDP, LOW);//segment DP high
+delay(1000);//segment A high
 //lletra e
-digitalWrite(segA, HIGH);
-digitalWrite(segB, LOW);
-digitalWrite(segC, LOW);
-digitalWrite(segD, HIGH);
-digitalWrite(segE, HIGH);
-digitalWrite(segF, HIGH);
-digitalWrite(segG, HIGH);
-digitalWrite(segDP, LOW);
-delay(1000);
+digitalWrite(segA, HIGH);//segment A high
+digitalWrite(segB, LOW);//segment B high
+digitalWrite(segC, LOW);//segment C high
+digitalWrite(segD, HIGH);//segment D high
+digitalWrite(segE, HIGH);//segment E high
+digitalWrite(segF, HIGH);//segment F high
+digitalWrite(segG, HIGH);//segment G high
+digitalWrite(segDP, LOW);//segment DP high
+delay(1000);//segment A high
 //lletra M
-digitalWrite(segA, HIGH);
-digitalWrite(segB, LOW);
-digitalWrite(segC, HIGH);
-digitalWrite(segD, LOW);
-digitalWrite(segE, HIGH);
-digitalWrite(segF, LOW);
-digitalWrite(segG, LOW);
-digitalWrite(segDP, LOW);
-delay(1000);
+digitalWrite(segA, HIGH);//segment A high
+digitalWrite(segB, LOW);//segment B high
+digitalWrite(segC, HIGH);//segment C high
+digitalWrite(segD, LOW);//segment D high
+digitalWrite(segE, HIGH);//segment E high
+digitalWrite(segF, LOW);//segment F high
+digitalWrite(segG, LOW);//segment G high
+digitalWrite(segDP, LOW);//segment DP high
+delay(1000);//segment A high
 }
