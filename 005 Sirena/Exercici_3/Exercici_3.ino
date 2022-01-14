ae3139a5a95ecb4f0c4a75bdec4fb9ab528043fa
guillemnunez2021 <guillem.nunez.2130@lacetania.cat>
2022-01-14 16:19:48 +0100

    exercici 3

--- 005 Sirena/Exercici_3/Exercici_3.ino
+++ 005 Sirena/Exercici_3/Exercici_3.ino
@@ -11,10 +11,13 @@
 const byte xiulet = 9;        // donar nom al pin 9 de l’Arduino
 const byte pot0 = A0;         // donar nom al pin A0 de l’Arduino
 int valPot0;                 // guardar valor del potenciometre  
+int valPot1;
+int valPot2;
 
 //********** Setup ****************************************************************
 void setup()
 {
+  Serial.begin(9600);
   pinMode(xiulet, OUTPUT);   // definir el pin 9 com una sortida
 }
 
@@ -22,19 +25,28 @@ void setup()
 void loop()
 {
   valPot0 = analogRead(pot0);    // llegir valor potenciòmetre 
+  valPot1 = valPot0 * 2;
+  valPot2 = valPot0 * 8;
 
   tone(xiulet, 1000, valPot0);    // xiulet de durada valPot0
-  delay(2*valPot0);              // esperar valPot0 del xiulet + valPot0 silenci
-  delay(1000);
+  delay(valPot1);              // esperar valPot0 del xiulet + valPot0 silenci
+ Serial.print("1 -> ");
+ Serial.println(valPot1);
+
   tone(xiulet, 1000, valPot0);    // xiulet de durada valPot0
-  delay(2*valPot0);              // esperar valPot0 del xiulet + valPot0 silenci
-  delay(1000);
+  delay(valPot1);              // esperar valPot0 del xiulet + valPot0 silenci
+  Serial.print("2 -> ");
+  Serial.println(valPot1);
+
   tone(xiulet, 1000, valPot0);    // xiulet de durada valPot0
-  delay(2*valPot0);              // esperar valPot0 del xiulet + valPot0 silenci
-  delay(1000);
+  delay(valPot1);              // esperar valPot0 del xiulet + valPot0 silenci
+  Serial.print("3 -> ");
+  Serial.println(valPot1);
+
   tone(xiulet, 1000, valPot0);    // xiulet de durada valPot0
-  delay(2*valPot0);              // esperar valPot0 del xiulet + valPot0 silenci
-  delay(1000);
+  delay(valPot2);              // esperar valPot0 del xiulet + valPot0 silenci
+  Serial.print("4 -> ");
+  Serial.println(valPot2);
 }
 
 //********** Funcions *************************************************************
